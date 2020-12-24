74b29719087e8240f89933be961d2385a4f6440f
slevanda <Disconnect62@gmail.com>
2020-12-24 12:08:08 +0300

    Update

---
+++ src/test/java/testpackage/steps/MOdetailedPageSteps.java
@@ -0,0 +1,58 @@
+package testpackage.steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import testpackage.pages.MOdetailedPage;
+
+public class MOdetailedPageSteps {
+    MOdetailedPage page;
+
+    @When("^Проверка отображения расписания на неделю у найденного МО$")
+    public void checkTableWeek() { page.checkTableWeek(); }
+
+    @When("^Проверка перехода к детальной информации найденного МО$")
+    public void checkTestMO() { page.checkTestMO(); }
+
+
+    @When("^Проверка отображения расписания на неделю внутри найденного МО$")
+    public void checkMOWeek() { page.checkMOWeek(); }
+
+    @When("^Проверка отображения услуги на неделю у найденного МО$")
+    public void checkServiceTableWeek() { page.checkServiceTableWeek(); }
+
+
+    @When("^Проверка отображения ссылки Расписание МО на месяц и перехода по ней$")
+    public void checkMOTableMonth() { page.checkMOTableMonth(); }
+
+
+    @When("^Проверка отображения Расписание услуги на месяц и перехода по ней$")
+    public void checkServiceTableMonth() { page.checkServiceTableMonth(); }
+
+    @When("^Проверка доступности заголовка открытого расписания МО$")
+    public void checkHeading() { page.checkHeading(); }
+
+    @When("^Проверка доступности блока выбора месяца в расписании МО$")
+    public void checkMonthChangeBlock() { page.checkMonthChangeBlock(); }
+
+    @When("^Проверка доступности кнопки выбора предыдущего месяца, у выбранного МО$")
+    public void checkMonthChangeLeftButton() { page.checkMonthChangeLeftButton(); }
+
+    @When("^Проверка отображения выбранного месяца, выбранного МО$")
+    public void checkMonthEnterAfterChange() { page.checkMonthEnterAfterChange(); }
+
+    @When("^Проверка доступности кнопки выбора следующего месяца, у выбранного МО$")
+    public void checkMonthChangeRightButton() { page.checkMonthChangeRightButton(); }
+
+    @When("^Проверка доступности календаря ранее выбранного МО$")
+    public void checkScheduleCalendar() { page.checkScheduleCalendar(); }
+
+    @When("^Проверка доступности кнопки открытия поп-апа календаря, у выбранного МО$")
+    public void checkCalendarButton() { page.checkCalendarButton(); }
+
+    @When("^Проверка доступности календаря поп-апа, у выбранного МО$")
+    public void checkPopApCalendar() { page.checkPopApCalendar(); }
+
+    @When("^Проверка доступности кнопки возврата к предпросмотру МО$")
+    public void checkBackButton() { page.checkBackButton(); }
+
+}
